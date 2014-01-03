dee5153c270b4d60866522758b2bc8ac754f786a
Matthew Batchelder <borkweb@gmail.com>
2014-01-03 11:44:24 -0500

    auto-loop through all the things

--- go-events-import.php
+++ go-events-import.php
@@ -6,9 +6,9 @@ define( 'WP_ADMIN', true );
 
 $options = array(
 	'wp::',
-	'event:',
-	'blog:',
-	'type:',
+	'event::',
+	'blog::',
+	'type::',
 );
 
 $args = getopt( '', $options );
@@ -29,24 +29,102 @@ require_once __DIR__ . '/components/class-go-events-import-sponsors.php';
 require_once __DIR__ . '/components/class-go-events-import-speakers.php';
 require_once __DIR__ . '/components/class-go-events-import-sessions.php';
 
-if ( 'sponsor' == $args['type'] || 'sponsors' == $args['type'] )
+fwrite( STDOUT, 'Loading manifest' );
+
+$handle = fopen( 'csv/event-manifest.csv', 'r' );
+$columns = array();
+$column_indexes = array();
+$column_keys = array();
+
+if ( FALSE === $handle )
 {
-	go_events_import_sponsors()->type = $args['type'];
-	go_events_import_sponsors()->load_event( $args['event'] );
-	go_events_import_sponsors()->file = $args['event'] . '-sponsors.csv';
-	go_events_import_sponsors()->process();
+	fwrite( STDOUT, 'Cound not open event manifest file' );
+	die;
 }//end if
-elseif ( 'speaker' == $args['type'] || 'speakers' == $args['type'] )
+
+for ( $count = 0; $line = fgetcsv( $handle, 0, ',' ); $count++ )
 {
-	go_events_import_speakers()->type = $args['type'];
-	go_events_import_speakers()->load_event( $args['event'] );
-	go_events_import_speakers()->file = $args['event'] . '-speakers.csv';
-	go_events_import_speakers()->process();
-}//end if
-elseif ( 'session' == $args['type'] || 'schedule' == $args['type'] )
+	fwrite( STDOUT, print_r( $line, TRUE ) );
+	if ( 0 == $count )
+	{
+		if ( ! is_array( $line ) || 0 == count( $line ) )
+		{
+			fwrite( STDOUT, 'Could not read manifest file columns' );
+			die;
+		}//end if
+
+		$keys = array_keys( $line );
+		$values = array_values( $line );
+
+		$column_indexes = array_combine( $values, $keys );
+		$column_keys = array_combine( $keys, $values );
+		continue;
+	}//end if
+
+	fwrite( STDOUT, print_r( $line, TRUE ) );
+
+	$event_name = $line[ $column_indexes[ 'event' ] ];
+	$event_slug = $line[ $column_indexes[ 'slug' ] ];
+	$event_start = $line[ $column_indexes[ 'start' ] ];
+	$event_end = $line[ $column_indexes[ 'end' ] ];
+
+	if ( ! $event_start )
+	{
+		continue;
+	}//end if
+
+	$event_start = explode( '/', $event_start );
+	$event_start = $event_start[2] . '-' . str_pad( $event_start[0], 2, '0', STR_PAD_LEFT ) . '-' . str_pad( $event_start[1], 2, '0', STR_PAD_LEFT ) . ' 12:00:00 AM';
+
+	$event_end = explode( '/', $event_end );
+	$event_end = $event_end[2] . '-' . str_pad( $event_end[0], 2, '0', STR_PAD_LEFT ) . '-' . str_pad( $event_end[1], 2, '0', STR_PAD_LEFT ) . ' 11:00:00 PM';
+
+	$args = array(
+		'name' => $event_slug,
+		'post_status' => array( 'publish', 'future' ),
+		'post_type' => 'go-events-event',
+	);
+
+	$query = new WP_Query( $args );
+
+	fwrite( STDOUT, print_r( $query->posts, TRUE ) );
+
+	if ( ! $query->posts )
+	{
+		$post = array(
+			'post_title' => $event_name,
+			'post_name' => $event_slug,
+			'post_status' => 'publish',
+			'post_type' => go_events()->event()->post_type_name,
+		);
+
+		$post_id = wp_insert_post( $post );
+
+		if ( ! $post_id )
+		{
+			fwrite( STDOUT, 'Unable to create the ' . $event_name . ' event post' );
+			continue;
+		}//end if
+
+		$meta = array(
+			'start' => $event_start,
+			'end' => $event_end,
+		);
+
+		go_events()->event()->admin()->update_meta( $post_id, $meta );
+
+		event_import( 'sponsors', $event_slug );
+		event_import( 'speakers', $event_slug );
+		event_import( 'schedule', $event_slug );
+	}//end if
+}//end for
+fclose( $handle );
+
+function event_import( $type, $event )
 {
-	go_events_import_sessions()->type = $args['type'];
-	go_events_import_sessions()->load_event( $args['event'] );
-	go_events_import_sessions()->file = $args['event'] . '-schedule.csv';
-	go_events_import_sessions()->process();
-}//end if
+	$function = 'go_events_import_' . ( 'schedule' == $type ? 'sessions' : $type );
+	$function()->type = $type;
+	$function()->load_event( $event );
+	$function()->file = "{$event}-{$type}.csv";
+	$function()->process();
+}//end event_import
